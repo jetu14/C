5743cd98f98198662c67727e9325af4071206eff
Nayem IslamJetu <nayemislamjetu@gmail.com>
2026-01-27 01:37:45 +0600

    Add files via upload

---
+++ CallByReference.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void swap(int *i,int *j){
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+    printf("After swap.\na:%d , b:%d", *i, *j);
+}
+
+int main(){
+    int a = 5,b = 7;
+    printf("Before swap.\na:%d , b:%d\n", a, b);
+    swap(&a,&b);
+}
+
